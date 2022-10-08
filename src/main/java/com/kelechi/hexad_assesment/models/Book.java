78733a167547ac3970b46a5d7bee6e6c0f76f3e8
Kenoly <kelechi.a@spinel.consulting>
2022-10-08 23:20:06 +0100

    Added book model and lombok dependency

---
+++ src/main/java/com/kelechi/hexad_assesment/models/Book.java
@@ -0,0 +1,12 @@
+package com.kelechi.hexad_assesment.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+}
--- src/test/java/com/kelechi/hexad_assesment/BookControllerTest.java
+++ src/test/java/com/kelechi/hexad_assesment/BookControllerTest.java
@@ -1,9 +1,11 @@
 package com.kelechi.hexad_assesment;
 
+import com.kelechi.hexad_assesment.models.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest
 public class BookControllerTest {
@@ -11,5 +13,7 @@ public class BookControllerTest {
     @Test
     void getAllBooks(){
         List<Book> books = new ArrayList<>();
+        books.add(new Book(1L, "Harry Potter", "JK Rowlings"));
+        books.add(new Book(2L, "Animal Farm", "George Owel"));
     }
 }
