a854b50779de84027e0aa4b0a851ba34e27e862a
Kenoly <kelechi.a@spinel.consulting>
2022-10-08 23:09:37 +0100

    first commit

---
+++ src/main/java/com/kelechi/hexad_assesment/HexadAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.kelechi.hexad_assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexadAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexadAssesmentApplication.class, args);
+	}
+
+}
