cb7334c78156bef3de7a0ef4039193662ddd92bf
Kenoly <kelechi.a@spinel.consulting>
2022-10-08 23:15:45 +0100

    Controller test for get all books feature

---
+++ src/test/java/com/kelechi/hexad_assesment/BookControllerTest.java
@@ -0,0 +1,13 @@
+package com.kelechi.hexad_assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+public class BookControllerTest {
+
+    @Test
+    void getAllBooks(){
+
+    }
+}
