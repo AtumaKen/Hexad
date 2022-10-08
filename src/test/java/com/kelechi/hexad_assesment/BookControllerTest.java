50ae1c572f2ca63643c0c55847fd8024162fd681
Kenoly <kelechi.a@spinel.consulting>
2022-10-08 23:18:06 +0100

    initialised list of books in test case

--- src/test/java/com/kelechi/hexad_assesment/BookControllerTest.java
+++ src/test/java/com/kelechi/hexad_assesment/BookControllerTest.java
@@ -3,11 +3,13 @@ package com.kelechi.hexad_assesment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import java.util.ArrayList;
+
 @WebMvcTest
 public class BookControllerTest {
 
     @Test
     void getAllBooks(){
-
+        List<Book> books = new ArrayList<>();
     }
 }
