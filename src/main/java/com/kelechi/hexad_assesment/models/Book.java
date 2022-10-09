fca4d4b6761ee0b17da66233db51cd6847f3ce19
Kenoly <kelechi.a@spinel.consulting>
2022-10-09 23:04:14 +0100

    test passed for cannotHaveNegativeNumberAsAvailableCopy

--- src/main/java/com/kelechi/hexad_assesment/models/Book.java
+++ src/main/java/com/kelechi/hexad_assesment/models/Book.java
@@ -1,13 +1,57 @@
 package com.kelechi.hexad_assesment.models;
 
+import com.kelechi.hexad_assesment.exceptions.ProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     private Long id;
     private String title;
     private String author;
     private int availableCopies;
+
+    public Book(Long id, String title, String author, int availableCopies) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        if(availableCopies < 0)
+            throw new ProcessingException("Available copies must be a positive value");
+        this.availableCopies = availableCopies;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        if(availableCopies < 0)
+            throw new ProcessingException("Available copies must be a positive value");
+        this.availableCopies = availableCopies;
+    }
 }
