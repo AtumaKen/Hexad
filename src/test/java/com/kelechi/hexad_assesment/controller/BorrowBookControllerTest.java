f516cfe3ca4cb3bf7e487c2842744c87047930b6
Kenoly <kelechi.a@spinel.consulting>
2022-10-09 23:48:16 +0100

    undo refactor

--- src/test/java/com/kelechi/hexad_assesment/controller/BorrowBookControllerTest.java
+++ src/test/java/com/kelechi/hexad_assesment/controller/BorrowBookControllerTest.java
@@ -63,7 +63,7 @@ public class BorrowBookControllerTest {
 
         when(borrowBookService.getBorrowedBooks()).thenReturn(books);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/borrow").content("1")
+        mockMvc.perform(MockMvcRequestBuilders.get("/borrow")
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(jsonPath("$", hasSize(2))).andDo(print());
     }
