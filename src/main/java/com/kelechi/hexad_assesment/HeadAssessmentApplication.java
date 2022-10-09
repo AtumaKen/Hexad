8a5f4abc52666cabb93f1dd3151f8d3183ab9824
Kenoly <kelechi.a@spinel.consulting>
2022-10-09 09:59:11 +0100

    require method for adding books

--- src/main/java/com/kelechi/hexad_assesment/HeadAssessmentApplication.java
+++ src/main/java/com/kelechi/hexad_assesment/HeadAssessmentApplication.java
@@ -1,7 +1,10 @@
 package com.kelechi.hexad_assesment;
-
+import com.kelechi.hexad_assesment.models.Book;
+import com.kelechi.hexad_assesment.services.BookService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HeadAssessmentApplication {
@@ -9,5 +12,7 @@ public class HeadAssessmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HeadAssessmentApplication.class, args);
 	}
+
+
 
 }
--- src/test/java/com/kelechi/hexad_assesment/services/BookServiceTest.java
+++ src/test/java/com/kelechi/hexad_assesment/services/BookServiceTest.java
@@ -4,13 +4,22 @@ import com.kelechi.hexad_assesment.models.Book;
 import com.kelechi.hexad_assesment.services.impl.BookServiceImpl;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookServiceTest {
 
     @Test
     void findAll(){
+
+        Book book1 = new Book(1L, "Harry Potter", "JK Rowlings");
+        Book book2  = new Book(2L, "Animal Farm", "George Owel");
+
         BookService service = new BookServiceImpl();
-        service.findAll();
+        service.add(book1);
+        service.add(book2);
+        List<Book> all = service.findAll();
     }
+
+
 }
