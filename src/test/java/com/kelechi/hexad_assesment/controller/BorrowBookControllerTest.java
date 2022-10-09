0c1be54e0182cc003a3df1eb81e35dfbd90e4b0f
Kenoly <kelechi.a@spinel.consulting>
2022-10-09 11:02:46 +0100

    added borrow book controller test

--- src/test/java/com/kelechi/hexad_assesment/controller/BorrowBookControllerTest.java
+++ src/test/java/com/kelechi/hexad_assesment/controller/BorrowBookControllerTest.java
@@ -1,16 +1,45 @@
 package com.kelechi.hexad_assesment.controller;
 
 
+import com.kelechi.hexad_assesment.models.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest
 public class BorrowBookControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BorrowService borrowService;
+
+
     @Test
-    void borrowBookControllerTest(){
+    void borrowBookControllerTest() throws Exception {
+        Book book1 = new Book(1L, "Harry Potter", "JK Rowlings");
+        Book book2  = new Book(2L, "Animal Farm", "George Owel");
+
+        List<Book> books = List.of(book1, book2);
+
+        when(borrowService.borrow(1)).thenReturn(books.remove(book1));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(jsonPath("$", hasSize(books.size() -1))).andDo(print());
 
     }
 }
