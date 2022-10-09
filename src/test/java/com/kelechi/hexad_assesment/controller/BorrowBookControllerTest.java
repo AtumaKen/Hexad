a40ea1322cc55657d270a534598cf69bb26b6f8b
Kenoly <kelechi.a@spinel.consulting>
2022-10-09 10:41:10 +0100

    set up BorrowBookControllerTest

---
+++ src/test/java/com/kelechi/hexad_assesment/controller/BorrowBookControllerTest.java
@@ -0,0 +1,16 @@
+package com.kelechi.hexad_assesment.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import java.util.List;
+
+@WebMvcTest
+public class BorrowBookControllerTest {
+
+    @Test
+    void borrowBookControllerTest(){
+
+    }
+}
