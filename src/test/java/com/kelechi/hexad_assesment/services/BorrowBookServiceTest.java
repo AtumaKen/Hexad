e95c9363eabf68dba1cb99a2f973a2536a028e6e
Kenoly <kelechi.a@spinel.consulting>
2022-10-09 16:19:39 +0100

    implemented test for bookLeavesTheLibraryWhenBorrowed

--- src/test/java/com/kelechi/hexad_assesment/services/BorrowBookServiceTest.java
+++ src/test/java/com/kelechi/hexad_assesment/services/BorrowBookServiceTest.java
@@ -1,18 +1,41 @@
 package com.kelechi.hexad_assesment.services;
 
+import com.kelechi.hexad_assesment.HeadAssessmentApplication;
 import com.kelechi.hexad_assesment.models.Book;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = HeadAssessmentApplication.class)
 class BorrowBookServiceTest {
+
+    @Autowired
+    private BorrowBookService service;
+
+    @Autowired
+    private BookService bookService;
 
     @Test
     void libraryIsNotEmptyBeforeBorrowing(){
 
 
+    }
+
+    @Test
+    void bookLeavesTheLibraryWhenBorrowed(){
+        Book book1 = new Book(1L, "Harry Potter", "JK Rowlings");
+        Book book2  = new Book(2L, "Animal Farm", "George Owel");
+
+        List<Book> books = new ArrayList<>(List.of(book1, book2));
+        Book borrowedBook = books.get(0);
+        List<Book> available = service.borrow(borrowedBook.getId());
+        assertFalse(available.stream().anyMatch(availableBooks -> bookService.compareBooks()));
     }
 
 }
