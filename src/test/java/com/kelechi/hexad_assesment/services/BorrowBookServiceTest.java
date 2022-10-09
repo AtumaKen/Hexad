bab83a199cd9e15c609e8fb7e1b7cfa48ee059e8
Kenoly <kelechi.a@spinel.consulting>
2022-10-09 22:39:45 +0100

    refactor Borrow book service test and changed bookLeavesTheLibraryWhenBorrowed to more meaningful test case bookLeavesTheLibraryWhenLastCopyIsBorrowed

--- src/test/java/com/kelechi/hexad_assesment/services/BorrowBookServiceTest.java
+++ src/test/java/com/kelechi/hexad_assesment/services/BorrowBookServiceTest.java
@@ -30,12 +30,16 @@ class BorrowBookServiceTest {
     }
 
     @Test
-    void bookLeavesTheLibraryWhenBorrowed(){
+    void bookLeavesTheLibraryWhenLastCopyIsBorrowed(){
+        bookService.getAll().clear();
+        borrowBookService.getBorrowedBooks().clear();
         Book book1 = new Book(1L, "Harry Potter", "JK Rowlings", 1);
         Book book2  = new Book(2L, "Animal Farm", "George Owel", 1);
 
-        List<Book> books = new ArrayList<>(List.of(book1, book2));
-        Book borrowedBook = books.get(0);
+
+        bookService.addBook(book1);
+        bookService.addBook(book2);
+        Book borrowedBook = bookService.getAll().get(0);
         List<Book> available = borrowBookService.borrow(borrowedBook.getId());
         assertFalse(available.stream().anyMatch(availableBook -> bookService.compareBooks(availableBook, borrowedBook)));
     }
@@ -50,10 +54,16 @@ class BorrowBookServiceTest {
 
     @Test
     void userHasABorrowingLimitOfTwoBooks (){
+        bookService.getAll().clear();
+        borrowBookService.getBorrowedBooks().clear();
+
         Book book1 = new Book(28L, "Harry Potter", "JK Rowlings", 1);
         Book book2  = new Book(98L, "Animal Farm", "George Owel", 1);
+        Book book3  = new Book(1L, "Fools Die", "Mario Puzo", 1);
         bookService.addBook(book1);
         bookService.addBook(book2);
+        bookService.addBook(book3);
+
         borrowBookService.borrow(98L);
         borrowBookService.borrow(28L);
         assertThrows( ProcessingException.class, () -> borrowBookService.borrow(1L), "User has a borrowing limit of 2");
