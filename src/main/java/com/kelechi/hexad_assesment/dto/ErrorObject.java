2e369cb3bebcf46d12b1bb42ed7b1471f2a51ab4
Kenoly <kelechi.a@spinel.consulting>
2022-10-10 16:13:02 +0100

    Error message object

---
+++ src/main/java/com/kelechi/hexad_assesment/dto/ErrorObject.java
@@ -0,0 +1,8 @@
+package com.kelechi.hexad_assesment.dto;
+
+import lombok.Data;
+
+@Data
+public class ErrorObject {
+    private String message;
+}
--- src/main/java/com/kelechi/hexad_assesment/rest_exception_handler/RestExceptionHandler.java
+++ src/main/java/com/kelechi/hexad_assesment/rest_exception_handler/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kelechi.hexad_assesment.rest_exception_handler;
 
+import com.kelechi.hexad_assesment.dto.ErrorObject;
 import com.kelechi.hexad_assesment.exceptions.ProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,7 +13,9 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(ProcessingException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String HandleProcessingException(ProcessingException ex){
-        return ex.getMessage();
+    public ErrorObject HandleProcessingException(ProcessingException ex){
+        ErrorObject errorObject = new ErrorObject();
+         errorObject.setMessage(ex.getMessage());
+         return errorObject;
     }
 }
