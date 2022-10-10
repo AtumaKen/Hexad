9e39a74f2a9fb70a0041bbd5ae61459b1ac2a27d
Kenoly <kelechi.a@spinel.consulting>
2022-10-10 15:22:00 +0100

    exception handler fix

--- src/main/java/com/kelechi/hexad_assesment/exceptions/ProcessingException.java
+++ src/main/java/com/kelechi/hexad_assesment/exceptions/ProcessingException.java
@@ -1,6 +1,11 @@
 package com.kelechi.hexad_assesment.exceptions;
 
+import lombok.Data;
+
+@Data
 public class ProcessingException extends RuntimeException {
+    private String message;
     public ProcessingException(String message) {
+        this.message = message;
     }
 }
--- src/main/java/com/kelechi/hexad_assesment/rest_exception_handler/RestExceptionHandler.java
+++ src/main/java/com/kelechi/hexad_assesment/rest_exception_handler/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.kelechi.hexad_assesment.rest_exception_handler;
 
 import com.kelechi.hexad_assesment.exceptions.ProcessingException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
