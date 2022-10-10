dbb99e88b8d62c97ccf96c24b0ea80bae1620310
Kenoly <kelechi.a@spinel.consulting>
2022-10-10 08:01:33 +0100

    test case set up for userCanReturnABookFromTheBorrowedList

--- src/test/java/com/kelechi/hexad_assesment/services/BookServiceTest.java
+++ src/test/java/com/kelechi/hexad_assesment/services/BookServiceTest.java
@@ -2,21 +2,30 @@ package com.kelechi.hexad_assesment.services;
 
 import com.kelechi.hexad_assesment.HeadAssessmentApplication;
 import com.kelechi.hexad_assesment.models.Book;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(classes = HeadAssessmentApplication.class)
 public class BookServiceTest {
 
     @Autowired
-    private BookService service;
+    private BookService bookService;
+
+    @Autowired
+    private BorrowBookService borrowBookService;
+
+    @BeforeEach
+    public void setUp() {
+        bookService.getAll().clear();
+        borrowBookService.getBorrowedBooks().clear();
+    }
 
     @Test
     void findAllBooks(){
@@ -25,9 +34,9 @@ public class BookServiceTest {
         Book book2  = new Book(2L, "Animal Farm", "George Owel", 1);
 
 
-        service.addBook(book1);
-        service.addBook(book2);
-        List<Book> allBooks = service.getAll();
+        bookService.addBook(book1);
+        bookService.addBook(book2);
+        List<Book> allBooks = bookService.getAll();
 
         Book lastBook = allBooks.get(allBooks.size() - 1);
         assertEquals(book2.getId(), lastBook.getId());
@@ -38,11 +47,24 @@ public class BookServiceTest {
     @Test
     void libraryIsNotEmptyAfterAddingBook(){
         Book book1 = new Book(1L, "Harry Potter", "JK Rowlings", 1);
-        service.addBook(book1);
-        assertFalse(service.getAll().isEmpty());
+        bookService.addBook(book1);
+        assertFalse(bookService.getAll().isEmpty());
     }
 
-
+    @Test
+    void userCanReturnABookFromTheBorrowedList(){
+        Book book1 = new Book(1L, "Harry Potter", "JK Rowlings", 1);
+        Book book2  = new Book(2L, "Animal Farm", "George Owel", 1);
+        bookService.addBook(book1);
+        bookService.addBook(book2);
+        borrowBookService.borrow(1L);
+        borrowBookService.borrow(2L);
+        bookService.returnBook(borrowBookService.findById(1L));
+        List<Book> availableBooks = bookService.getAll();
+        List<Book> borrowedBooks = borrowBookService.getBorrowedBooks();
+        assertTrue(availableBooks.stream().anyMatch(available -> bookService.compareBooks(available, book1)));
+        assertFalse(borrowedBooks.stream().anyMatch(borrwed -> bookService.compareBooks(borrwed, book1)));
+    }
 
 
 
